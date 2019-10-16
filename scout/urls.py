9ac299553de15b009ec541e2ce52a247f554e5c4
Zsolt Endreffy <zsolt.endreffy@balabit.com>
2019-10-16 18:21:04 +0200

    redirect root to merchant_game

--- scout/urls.py
+++ scout/urls.py
@@ -14,9 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import include, path
+
+
+def redirect_to_merchant_game(request):
+    return redirect('merchant_game:index', permanent=True)
+
 
 urlpatterns = [
     path('merchant_game/', include('merchant_game.urls')),
     path('admin/', admin.site.urls),
+    path('', redirect_to_merchant_game),
 ]
