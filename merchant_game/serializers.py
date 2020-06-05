bcac207238d13f6d77fb4c37cf009f05b9ac7b7a
dyj216 <ifjezsolt@gmail.com>
2020-06-05 02:35:58 +0200

    change the items serializer in the PlayerSerializer
    Change from SerializerMethodField to a custom ItemsField

--- merchant_game/serializers.py
+++ merchant_game/serializers.py
@@ -1,31 +1,47 @@
-from django.db.models import Max
+from django.core import exceptions
 from rest_framework import serializers
 
-from .models import Player, City, ItemExchangeRate, ItemAmount, Item
+from .models import Player, City, ItemExchangeRate, ItemAmount
+
+
+class ItemsField(serializers.Field):
+    default_error_messages = {
+        "non_existing_item": "Item does not exist: {item_name}",
+        "negative_value": "Value is too low. Ensure it is higher than 0",
+    }
+
+    def get_attribute(self, instance):
+        return instance.items.all()
+
+    def to_representation(self, value):
+        return {item.item.name: item.amount for item in value}
+
+    def to_internal_value(self, data):
+        items = []
+        for item_name, amount in data.items():
+            try:
+                item = ItemAmount.objects.get(player=self.parent.instance, item=item_name)
+            except exceptions.ObjectDoesNotExist:
+                self.fail("non_existing_item", item_name=item_name)
+            if amount < 0:
+                self.fail("negative_value")
+            item.amount = amount
+            items.append(item)
+        return items
 
 
 class PlayerSerializer(serializers.HyperlinkedModelSerializer):
-    items = serializers.SerializerMethodField()
+    items = ItemsField()
 
     class Meta:
         model = Player
         fields = ['url', 'code', 'money', 'items']
 
-    @staticmethod
-    def get_items(player):
-        return {
-            item.item.name: item.amount for item in player.items.all()
-        }
-
     def update(self, instance, validated_data):
         super().update(instance, validated_data)
-
-        items = self.initial_data.get("items", None)
-        if items:
-            for item_name, amount in items.items():
-                item = ItemAmount.objects.get(player=instance, item=Item.objects.get(name=item_name))
-                item.amount = amount
-                item.save()
+        items = validated_data.get("items", [])
+        for item in items:
+            item.save()
         return instance
 
 
